70a84487ca7cbd568fbea7471e953907f4dcf980
camyacodes <camyacodes@gmail.com>
2021-12-28 10:42:40 -0500

    fetch weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-var getCityWeather = function() {
-    console.log("function was called");
-  };
-  
-getCityWeather();
+var getCityCoord = function (city) {
+	var API_Key = "7944758690feb8acd835037db2bb2590";
+	// format the open weather api url
+	var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`;
+
+	fetch(apiUrl).then(function (response) {
+		response.json().then(function (data) {
+			console.log(data);
+		});
+	});
+};
+
+getCityCoord("london");
